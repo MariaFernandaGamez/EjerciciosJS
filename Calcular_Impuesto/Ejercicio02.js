3f197858ed7b4f7f4f526e070c4488d13c4c71ab
MariaFernandaGamez <marfgamez04@gmail.com>
2024-04-23 07:07:00 -0500

    Arreglos Ejercicio 2 :art:

--- Calcular_Impuesto/Ejercicio02.js
+++ Calcular_Impuesto/Ejercicio02.js
@@ -1,5 +1,4 @@
 function calcularImpuestos (edad, ingresos) {
-    
     if (edad >=18 && ingresos>=1000) {
         let impuesto= ingresos*0.40;
         return impuesto;
@@ -12,3 +11,7 @@ let edad= parseInt(prompt('Ingrese su edad'));
 let ingresos= parseFloat(prompt('Cantidad de ingresos'));
 let impuestoCalculado=calcularImpuestos(edad, ingresos);
 alert("Sus impuestos son: " + impuestoCalculado);
+
+
+
+
