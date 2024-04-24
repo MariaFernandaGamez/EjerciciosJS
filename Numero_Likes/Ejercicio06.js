dfb8f2ee3903eb3c5e705562b83ce83f5a990fed
MariaFernandaGamez <marfgamez04@gmail.com>
2024-04-24 06:31:21 -0500

    Tercer ejercicio :sparkles:

---
+++ Numero_Likes/Ejercicio06.js
@@ -0,0 +1,23 @@
+// function likes(number) {
+//     if (number >= 1000000){
+//         return Math.floor(number/1000000) +'M';
+//     } else if (number >=1000) {
+//         return Math.floor(number/1000) +'K';
+//     } else {
+//         return number.toString();
+//     }
+// }
+
+// let number=parseInt(prompt('Ingrese el numero de likes'));
+// numeroLikes = likes(number);
+// alert('la cantidad de likes es: ' +numeroLikes);
+
+
+// /*
+//     --Math.floor(seguida del numero)-- 
+//         convierte el resultado de cualquier operacion a numero entero.
+//     --variable.toString()--
+//         convierte cualquier variable en string
+// */
+
+
